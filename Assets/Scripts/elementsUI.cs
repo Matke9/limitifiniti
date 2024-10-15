68dd02fa83fa51587ac1c5782914ecf51024a83e
Vojin <isusgradiprozore@gmail.com>
2024-10-15 21:43:10 +0200

    Big meni
    Matovicu stisni play

--- Assets/Scripts/elementsUI.cs
+++ Assets/Scripts/elementsUI.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Add this for UI components
 
 public class elementsUI : MonoBehaviour
 {
+    
+   
+
     public void izadji()
     {
         Application.Quit();
     }
+
+    
 }
