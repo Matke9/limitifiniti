6c1afc25b53f4d2f8443078a44caf132f7ed1fed
Matke9 <matovicmihailo@gmail.com>
2024-10-17 21:31:22 +0200

    merge

---
+++ Assets/Scripts/Building/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera camera;
+    [SerializeField]
+    Transform gridShader;
+    [SerializeField]
+    Transform player;
+    float zoomTarget;
+
+    [SerializeField]
+    float multiplier = 2, minZoom = 1, maxZoom = 10, smoothTime = .1f;
+    float velocity = 0;
+
+    enum CamMode
+    {
+        Player,
+        Build,
+        Combat
+    }
+
+    CamMode camMode;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        zoomTarget = camera.orthographicSize;
+    }
+
+    void Update()
+    {
+        if (camMode == CamMode.Build)
+        {
+            zoomTarget -= Input.GetAxisRaw("Mouse ScrollWheel") * multiplier;
+            zoomTarget = Mathf.Clamp(zoomTarget, minZoom, maxZoom);
+            camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomTarget, ref velocity, smoothTime);
+            int gridSize = Mathf.RoundToInt(camera.orthographicSize) * 4 + 1;
+            gridShader.localScale = new Vector3(gridSize, gridSize, 1);
+        }
+        else if (camMode == CamMode.Player)
+        {
+            camera.transform.position = player.position;
+        }
+    }
+}
