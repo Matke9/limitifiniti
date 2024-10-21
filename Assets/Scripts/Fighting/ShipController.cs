3b389cdf15a0cac62c8a08319172106dff216e96
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2024-10-21 11:02:41 +0200

    Sounds

--- Assets/Scripts/Fighting/ShipController.cs
+++ Assets/Scripts/Fighting/ShipController.cs
@@ -14,20 +14,42 @@ public class ShipController : MonoBehaviour
     private float moveInput;
     private float steerInput;
 
+    private AudioSource engineSound;
+    public float minVolume = 0.01f; // Default volume when throttle is zero
+    public float maxVolume = 0.06f; // Maximum volume when throttle is full
+    public float pitchRange = 0.1f; // Pitch range increase based on throttle
+    private float throttle; // Represents the throttle amount
+
     CameraController camControler;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         camControler = Camera.main.GetComponent<CameraController>();
+        engineSound = GetComponents<AudioSource>()[1];
     }
 
     private void Update()
     {
         if (camControler.camMode == CameraController.CamMode.Combat)
         {
+            if (!engineSound.isPlaying)
+            {
+                engineSound.Play();
+            }
+
             moveInput = Input.GetAxisRaw("Vertical");
             steerInput = Input.GetAxisRaw("Horizontal");
+
+            // Calculate throttle as the absolute value of move input to get a positive range
+            throttle = Mathf.Clamp(Mathf.Abs(moveInput), 0f, 1f);
+
+            // Smoothly change the pitch based on the throttle value
+            engineSound.pitch = Mathf.Lerp(engineSound.pitch, 1f + throttle * pitchRange, Time.deltaTime * 2f);
+
+            // Smoothly change the volume based on the throttle value
+            float targetVolume = Mathf.Lerp(minVolume, maxVolume, throttle);
+            engineSound.volume = Mathf.Lerp(engineSound.volume, targetVolume, Time.deltaTime * 2f);
         }
     }
 
--- Assets/Scripts/Fighting/Shooting.cs
+++ Assets/Scripts/Fighting/Shooting.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,17 +7,35 @@ public class Shooting : MonoBehaviour
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletSpeed = 10f;
     [SerializeField] private float fireCooldown = 1f;
+    [SerializeField] private byte gunType;
     private CameraController camController;
+    private AudioSource laserSource;
+    private List<AudioClip> audioClips;
 
     private float timeSinceLastShot;
     void Start()
     {
         camController = Camera.main.GetComponent<CameraController>();
+        laserSource = transform.parent.parent.GetComponents<AudioSource>()[0];
+        if (gunType == 0)
+        {
+            audioClips = transform.parent.parent.GetComponent<PlayerSounds>().railGunSounds;
+        }
+        else if (gunType == 1) 
+        {
+            audioClips = transform.parent.parent.GetComponent<PlayerSounds>().rapidCanonSounds;
+        }
+        else
+        {
+            audioClips = transform.parent.parent.GetComponent<PlayerSounds>().pulseCanonSounds;
+        }
     }
     private void Update()
     {
         if (Input.GetMouseButton(0) && timeSinceLastShot >= fireCooldown && camController.camMode == CameraController.CamMode.Combat)
         {
+            laserSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count)]);
+            laserSource.pitch = Random.Range(0.8f, 1.1f);
             Shoot();
             timeSinceLastShot = 0f;
         }
