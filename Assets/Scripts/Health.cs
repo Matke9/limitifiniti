af62af15e099b676747ead7d9e08f6e5307f25cc
Aleksandar Radojicic <63604260+Aleksandar-Radojicic@users.noreply.github.com>
2024-10-21 09:27:58 +0200

    stvari

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,12 +6,23 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public int maxHealth = 10, currentHealth = 0;
+    PlacementSystem buildSys;
     void Start()
     {
+        buildSys = GameObject.FindGameObjectWithTag("BuildSys").transform.GetComponent<PlacementSystem>();
         currentHealth = maxHealth;
     }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        if (transform.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        } 
+        if (transform.tag == "ShipPart")
+        {
+            buildSys.shipController.moveSpeed = buildSys.shipController.speedMultiplier * buildSys.blockData.GetThrusterRatio();
+            Destroy(gameObject);
+        }
     }
 }
